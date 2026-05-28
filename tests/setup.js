55fb4ab208edd99ac7eb6bc3a327516f22a2edeb
Felix Hoffmann <fx.hoffmann93@gmail.com>
2026-05-28 13:04:36 +0200

    chore(setup): remove name prefix and Instagram delay configuration
    - Remove nameLowercasePrefixes global configuration from test setup
    - Remove instagramCheckDelay global configuration from test setup
    - Simplify test configuration by removing configurable options that are no longer needed

--- tests/setup.js
+++ tests/setup.js
@@ -36,8 +36,6 @@ global.defaultCountryCode = '+49';
 global.scheduleAutoLabeling = false;
 global.sendActionReports = true;
 global.nameSwapLastFirst = true;
-global.nameLowercasePrefixes = ['von', 'van', 'de', 'del', 'der', 'di', 'la', 'le', 'el'];
-global.instagramCheckDelay = 1000;
 
 // Mock Logger
 global.Logger = {
